2829391e79f061ed05d378a46064e0c7995e167a
smallflowercat1995 <smallflowercat1995@outlook.com>
2022-03-29 19:23:30 +0800

    提交提交数据脚本文件

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # clash-script-for-system
+Clash virtual private network technology script adapted to various systems
---
+++ arm64 clash for termux android/clash-update.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# ./wget --connect-timeout=15 --no-check-certificate "https://gitlab.com/free9999/ipupdate/-/raw/master/clash/config.yaml"  -O ".config/clash/config.yaml"
+curl "https://cdn.jsdelivr.net/gh/Alvin9999/pac2@latest/clash/3/config.yaml" -o "config1.yaml"
+curl "https://gitlab.com/free9999/ipupdate/-/raw/master/clash/config.yaml" -o "config2.yaml"
+curl "https://cdn.jsdelivr.net/gh/Alvin9999/pac2@latest/clash/2/config.yaml" -o "config3.yaml"
+curl "https://gitlab.com/free9999/ipupdate/-/raw/master/clash/3/config.yaml" -o "config4.yaml"
+curl "https://cdn.jsdelivr.net/gh/Alvin9999/pac2@latest/clash/config.yaml" -o "config5.yaml"
+curl "https://gitlab.com/free9999/ipupdate/-/raw/master/clash/2/config.yaml" -o "config6.yaml"
