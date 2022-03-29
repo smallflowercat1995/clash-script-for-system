6cc86c474f5bd7ddf1aa3f3e719b915754179aa4
smallflowercat1995 <smallflowercat1995@outlook.com>
2022-03-29 19:53:47 +0800

    提交提交数据脚本文件

--- amd64 clash for linux/clash-update.sh
+++ amd64 clash for linux/clash-update.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-# ./wget --connect-timeout=15 --no-check-certificate "https://gitlab.com/free9999/ipupdate/-/raw/master/clash/config.yaml" -O "config.yaml"
-./curl "https://gitlab.com/free9999/ipupdate/-/raw/master/clash/config.yaml" -o "config1.yaml"
-./curl "https://gitlab.com/free9999/ipupdate/-/raw/master/clash/3/config.yaml" -o "config2.yaml"
-./curl "https://gitlab.com/free9999/ipupdate/-/raw/master/clash/2/config.yaml" -o "config3.yaml"
-./curl "https://cdn.jsdelivr.net/gh/Alvin9999/pac2@latest/clash/config.yaml" -o "config4.yaml"
-./curl "https://cdn.jsdelivr.net/gh/Alvin9999/pac2@latest/clash/3/config.yaml" -o "config5.yaml"
-./curl "https://cdn.jsdelivr.net/gh/Alvin9999/pac2@latest/clash/2/config.yaml" -o "config6.yaml"
+export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7891
+curl "https://gitlab.com/free9999/ipupdate/-/raw/master/clash/config.yaml" -o "config1.yaml"
+curl "https://gitlab.com/free9999/ipupdate/-/raw/master/clash/3/config.yaml" -o "config2.yaml"
+curl "https://gitlab.com/free9999/ipupdate/-/raw/master/clash/2/config.yaml" -o "config3.yaml"
+curl "https://cdn.jsdelivr.net/gh/Alvin9999/pac2@latest/clash/config.yaml" -o "config4.yaml"
+curl "https://cdn.jsdelivr.net/gh/Alvin9999/pac2@latest/clash/3/config.yaml" -o "config5.yaml"
+curl "https://cdn.jsdelivr.net/gh/Alvin9999/pac2@latest/clash/2/config.yaml" -o "config6.yaml"
