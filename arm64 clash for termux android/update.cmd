7875404b0717795cf6a0b1300fb55357bd97a8a6
smallflowercat1995 <smallflowercat1995@outlook.com>
2022-03-29 19:58:53 +0800

    提交提交数据脚本文件

--- arm64 clash for termux android/update.cmd
+++
@@ -1,6 +0,0 @@
-curl "https://gitlab.com/free9999/ipupdate/-/raw/master/clash/config.yaml" -o "config1.yaml"
-curl "https://gitlab.com/free9999/ipupdate/-/raw/master/clash/3/config.yaml" -o "config2.yaml"
-curl "https://gitlab.com/free9999/ipupdate/-/raw/master/clash/2/config.yaml" -o "config3.yaml"
-curl "https://cdn.jsdelivr.net/gh/Alvin9999/pac2@latest/clash/config.yaml" -o "config4.yaml"
-curl "https://cdn.jsdelivr.net/gh/Alvin9999/pac2@latest/clash/3/config.yaml" -o "config5.yaml"
-curl "https://cdn.jsdelivr.net/gh/Alvin9999/pac2@latest/clash/2/config.yaml" -o "config6.yaml"
