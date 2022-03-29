6690022a76c5775a370d242d0601252b065bf9c0
smallflowercat1995 <smallflowercat1995@outlook.com>
2022-03-29 20:49:51 +0800

    提交提交数据脚本文件

--- amd64 clash for mac/clash-start.sh
+++ amd64 clash for mac/clash-start.sh
@@ -14,7 +14,7 @@ echo 等待软件启动，请稍候...
 nohup ./clash-darwin-amd64-v1.8.0 -d .config/clash/ > clash-darwin-amd64-v1.8.0.log 2>&1 &
 if [ -e /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome ];then 
 echo "true"
-nohup /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --user-data-dir=chromium-user-data --proxy-server=http://127.0.0.1:7890 https://www.duckduckgo.com/?q=GoClashB > chromium.log 2>&1 &
+nohup /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --user-data-dir=chrome-user-data --proxy-server=http://127.0.0.1:7890 https://www.duckduckgo.com/?q=GoClashB > chrome.log 2>&1 &
 else 
 echo Chrome浏览器不存在或没有正确安装，请尝试重新安装Chrome浏览器
 fi
