a586c98215bcbb85083156aff3724624647da769
smallflowercat1995 <smallflowercat1995@smallfl995deMBP.lan>
2022-04-25 16:44:29 +0800

    提交修改文档

--- amd64 clash for windows/.!34636!start.cmd
+++
@@ -1,7 +0,0 @@
-cls.
-@echo off
-color 0a
-rem %1 start "" mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
-CD /D "%~dp0"
-taskkill /f /t /im "clash-windows*"  >NUL 2>NUL
-taskkill /f /t /im "chrome*"  >NUL 2>NUL
